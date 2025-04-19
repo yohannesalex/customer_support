62c3b59b323aaa92b43844f28fbc7df19bb5df71
yohannesalex <yohannesalemayehu624@gmail.com>
2025-04-19 19:36:07 +0300

    fix alembic error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-# entrypoint.sh
-
-# Navigate to the directory containing alembic.ini
 cd support_assistant_backend
 
-# Run initial migrations
-alembic revision --autogenerate -m "Initial migration"
+# Generate migrations
+python -m alembic revision --autogenerate -m "Initial migration"
 
-alembic -c alembic.ini upgrade head
+# Run migrations
+python -m alembic upgrade head
 
-# Start the FastAPI application
+# Start the app
 exec uvicorn main:app --host 0.0.0.0 --port 9000 --reload
